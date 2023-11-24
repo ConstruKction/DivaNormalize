f228036402ae95ee30aa8e36593a7917a57f2565
ConstruKction <patdvkc@gmail.com>
2023-11-24 18:19:43 +0100

    Change LUFS default from -14 to -15.

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ MODS_DIR_PATH = Path('E:/SteamLibrary/steamapps/common/Hatsune Miku Project DIVA
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--lufs',
-                        help='LUFS threshold for loudness analysis. Default is -14.',
+                        help='LUFS threshold for loudness analysis. Default is -15.',
                         type=float,
-                        default=-14)
+                        default=-15)
     parser.add_argument('-s', '--sample-rate',
-                        help='Sample rate in Hz.',
+                        help='Sample rate in Hz. MM+ is 44100 and PDAFT is 48000.',
                         type=int,
                         default=44100)
     args = parser.parse_args()
