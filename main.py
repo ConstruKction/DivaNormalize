840f42434d044b430be2251633f2e1d00d593e26
ConstruKction <patdvkc@gmail.com>
2023-11-24 18:29:20 +0100

    Update .gitignore and add argument option for mod directory path

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 from audio_processor import AudioProcessor
 
-MODS_DIR_PATH = Path('E:/SteamLibrary/steamapps/common/Hatsune Miku Project DIVA Mega Mix Plus/mods_test')
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input',
+                        help='FULL PATH to your mods directory. Default is wherever the script is.',
+                        type=Path,
+                        default='.')
     parser.add_argument('-l', '--lufs',
                         help='LUFS threshold for loudness analysis. Default is -15.',
                         type=float,
@@ -17,7 +19,7 @@ if __name__ == '__main__':
                         default=44100)
     args = parser.parse_args()
 
-    audio_processor = AudioProcessor(MODS_DIR_PATH)
+    audio_processor = AudioProcessor(args.input)
     audio_processor.load_processed_files('processed_songs.json')
     audio_processor.find_oggs()
     audio_processor.process_oggs(args.lufs, args.sample_rate)
